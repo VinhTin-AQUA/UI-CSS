8bf8826a23a9f67df9528ff55f31f8e5af20f41c
Your Name <you@example.com>
2023-05-20 21:50:03 +0700

    DropdownMenu using css and javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import ModernCSSButtonHover from "./components/UI Components/ModernCSSButtonHover/ModernCSSButtonHover"
-
+//import ModernCSSButtonHover from "./components/UI Components/ModernCSSButtonHover/ModernCSSButtonHover"
+import DropdownMenu from "./components/UI Components/DropdownMenu"
 
 function App() {
 
   return (
     <div>
-      <ModernCSSButtonHover />
+      {/*<ModernCSSButtonHover />*/}
+      <DropdownMenu />
     </div>
   )
 }
